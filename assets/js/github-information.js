0ed9bfcdf4d7861a096ae902940ab4081b9dcc2e
Suciu Dan <suciu_t_dan@yahoo.com>
2024-06-12 11:09:42 +0000

    Add  promise using $.when.then to find a user

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -11,4 +11,25 @@ function fetchGitHubInformation(event) {
         `<div id="loader">
             <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
+
+    // Add a promise to find a user and release an error message if not found
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`)
+    ).then(
+        function(response) {
+            var userData = response;
+            $("#gh-user-data").html(userInformationHTML(userData));
+        }, 
+        function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        });
 }
+
+
