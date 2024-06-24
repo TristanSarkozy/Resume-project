6447deffa86dc7105ca6e6c2b4c977de692534d9
Suciu Dan <suciu_t_dan@yahoo.com>
2024-06-24 18:55:48 +0000

    Add function to send emails

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,17 @@
-console.log("Hello");
+// Add function to send emails
+function sendEmail(contactForm) {
+    emailjs.send("gmail", "rosie", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    // Add then method to our promise
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("Failed", error);
+        });
+        return false;
+}
